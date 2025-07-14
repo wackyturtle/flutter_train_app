0c7f2cc84b4139b11cf554fe25cfcb04b0d3036f
wackyturtle <wcytutl01@gmail.com>
2025-07-14 17:54:07 +0900

    Update: default layout

---
+++ lib/seat/widget/Seat_Page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SeatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('시트페이지 입니다');
+  }
+}
---
+++ lib/station/Station_List_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class StationListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('역 페이지 입니다');
+  }
+}
